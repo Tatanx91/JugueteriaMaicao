e2ba7cfe50b057c820cbcef2203e7d1e159d29ea
Leogomez091 <leogomez091@hotmail.com>
2018-03-10 14:17:16 -0500

    prueba del git ern view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Laravel</title>
-
+        <h3>prueba subir a github.com</h3>
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
 
